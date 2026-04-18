1dc168e23e938ba5f006169a8a2b1dfe844a066d
Khitiara <robotbrainify@gmail.com>
2026-04-18 17:06:38 -0400

    fix preferred optimize mode

--- build.zig
+++ build.zig
@@ -4,7 +4,8 @@ pub const BuildInterface = @import("src/BuildInterface.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSafe });
+    b.release_mode = .safe;
+    const optimize = b.standardOptimizeOption(.{});
 
     const test_step = b.step("test", "Runs the test suite.");
 
